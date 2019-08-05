44d01d90ab56196a049e01eb8b02995b702c05c4
webizfabulous <webizmoxx@gmail.com>
2019-08-05 13:31:56 +0545

    Added templates and new routes

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,30 @@
 
 from app import app #__init__.py
-
+from flask import render_template
 
 
 @app.route('/')
 def hello_world():
     return "<h1>Hello , World!</h1>"
+
+
+@app.route('/index')
+def index():
+    return render_template('index.htm' , login=False)
+
+
+@app.route('/courses')
+def courses():
+    return render_template('courses.htm' , login=False)
+
+
+@app.route('/register')
+def register():
+    return render_template('register.htm' , login=False)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.htm' , login=False)
 
 
